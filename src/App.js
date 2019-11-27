a508abea3806d44780ea52d657209f8c3e928281
mdz <mdz.dev24@gmail.com>
2019-11-27 19:29:39 +0100

    4 - ( React TodoList App ) Delete Item From TodoList 5 - ( React TodoList App ) Show No Items Message

--- src/App.js
+++ src/App.js
@@ -17,11 +17,33 @@ class  App extends Component {
 
 
 
+  deletItem = (id) => {
+    console.log(id);
+    let items = this.state.items;  //hna daba 7atit dik array items f1 variable smito items
+    let i = items.findIndex(item=>item.id === id); //hna daba galt lih dir boucle 3la array b dik finIndex o ligne li l9ayti item.id dyalo === dak id li dawazna lih 7ato f1  variable i
+    items.splice(i,1); //supprimer liya la ligne li l9iti
+    this.setState({items});
+
+
+
+    // deuxiemme méthode de suppression
+    // f had méthode kandir fonction filter li katdoz liya 3la chaque ligne o katakhod liya l id dyal chaque ligne o bine insertion o katchof wach dak id ma kaysawich id li 3tito o kaykhaliya o kay7ayad li kaysawi dak id
+
+    // let items = this.state.items.filter(item => {
+    //   return item.id != id;
+    // })
+
+    // this.setState(items);
+
+
+  }
+
+
   render(){
     return(
     <div className="App">
       <h1>Tod list App</h1>
-      <TodoItems vals_array = {this.state.items} />
+      <TodoItems vals_array = {this.state.items} deleteItem={this.deletItem} />
       <AddItem/>
     </div>
   );
--- src/components/TodoItems/TodoItems.js
+++ src/components/TodoItems/TodoItems.js
@@ -6,18 +6,27 @@ import './TodoItems.css'
 const TodoItems = (props) =>{   //(props) hadi kandirha wast fonction bach njib props dial had component(TodoItems) mn autre componenst(App.js)
 
         const {vals_array} = props; //val_array le nom dyal props li kayn f l'autre component f had l'exempls 3andna howa App.js
-       
-            const  ListeItems = vals_array.map( val =>{
+        const {deleteItem} = props;
 
+        let lengeur_array = vals_array.length;
+       
+            const  ListeItems = lengeur_array ? (
+            vals_array.map( val =>{
                 return (
                     <div key={val.id}>
                         <span>{val.name}</span>
                         <span>{val.age}</span>
-                        <span>x</span>
+                        {/* <span onClick={deleteItem(val.id)}>x</span>comme ca fonction ka it2éxécuta rasha pour éviter foction tdir execution tout seule kanzid arrow function */}
+                        <span onClick={()=>deleteItem(val.id)}>x</span> 
+                        {/* {()=>deleteItem(val.id)} Had kitaba kat3ni dart 1 fonction sans nom o bien renommage o hadik lfonction li sans nom ghada tdir liya run l fonction deleteItem*/}
                     </div>
                 )
 
             })
+            ):(
+                <p>There is no item to show</p>
+            )
+
        
        
          console.log(props);
